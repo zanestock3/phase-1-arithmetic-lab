93dfc31ac35a0f9aa6b6185a84d4e328e7d35351
Zane Stock <zanestock3@gmail.com>
2022-04-19 18:24:06 -0500

    commit tree 7471be11bcf0a43b76647fa2348e44adcb4be46d

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-
-// Write your code here
+{
+const num1 = 31;
+const num2 = 2;
+let multiply = num1 * num2
+}
+{
+let random = Math.floor(Math.random() * 5) + 2;
+}
+{
+    const num3 = 3;
+    const num4 = 1;
+    let mod = num3 + num4;
+}
+{
+let max = Math.max(5, 10, 20)
+}
